b386e9f46eb739ed225018d9731deed07e852088
Khuyen Tran <khuyentran1476@gmail.com>
2022-08-29 13:13:16 -0500

    init commit

---
+++ getting-started/01_no_prefect.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+# Imports for loading configuration
+from omegaconf import DictConfig
+from hydra import compose, initialize
+from sqlalchemy import create_engine
+
+def load_config():
+    with initialize(version_base=None, config_path="../config"):
+        config = compose(config_name="main")
+    return config
+
+
+def get_data(config: DictConfig):
+    connection = config.connection
+    engine = create_engine(
+        f"postgresql://{connection.user}:{connection.password}@{connection.host}/{connection.database}",
+    )
+    query = f'SELECT * FROM "{config.data.raw}"'
+    df = pd.read_sql(query, con=engine)
+    return df
+
+
+def fill_missing_description(data: pd.DataFrame):
+    data["Description"] = data["Description"].fillna("")
+    return data
+
+
+def get_desc_length(data: pd.DataFrame):
+    data["desc_length"] = data["Description"].str.len()
+    return data
+
+
+def process_data():
+    config = load_config()
+    data = get_data(config)
+    na_processed = fill_missing_description(data)
+    feature_added = get_desc_length(na_processed)
+
+
+if __name__ == "__main__":
+    process_data()
